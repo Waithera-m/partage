8ef0b95962f3baa5e7dea4dc3a4c625d96bfb690
Mary Waithera <njihiamary11@gmail.com>
2020-05-10 12:56:18 +0300

    create new config class and database to facilitate the testing of models' behavior

--- config.py
+++ config.py
@@ -8,14 +8,23 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     
     QUOTE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mary:pixie01@localhost/partage'
+    
 
 class DevConfig(Config):
 
     '''
     class inherits general configurations from Config class
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mary:pixie01@localhost/partage'
+
     DEBUG = True
+
+class TestConfig(Config):
+
+    '''
+    class inherits general configurations from config class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mary:pixie01@localhost/partage_test'
 
 class ProdConfig(Config):
 
@@ -26,5 +35,6 @@ class ProdConfig(Config):
 
 config_options = {
     'development':DevConfig,
-    'production':ProdConfig
+    'production':ProdConfig,
+    'test':TestConfig
 }
