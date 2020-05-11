b8854b2987ecbf99bd7eb2a418ae7dcd74f3b863
Mary Waithera <njihiamary11@gmail.com>
2020-05-11 14:09:29 +0300

    create post delete function

--- app/main/views.py
+++ app/main/views.py
@@ -154,4 +154,14 @@ def registered_posts(uname):
 
     return render_template('profile/blog_posts.html',user=user,posts=posts)
 
+@main.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
     
